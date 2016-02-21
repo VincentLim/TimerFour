0080d0aa600eeff26fd602c3d5b3930ed915174d
Vincent Limorté <vincent.limorte@gmail.com>
2016-02-21 14:52:24 +0100

    changed Timer3 to Timer4. Removed teensy and some other boards support

--- TimerFour.cpp
+++ TimerFour.cpp
@@ -6,6 +6,7 @@
  *  Modified Oct 2009 by Dan Clemens to work with timer3 of the ATMega1280 or Arduino Mega
  *  Modified April 2012 by Paul Stoffregen
  *  Modified again, June 2014 by Paul Stoffregen
+ *  Modified February 2016 by Vincent Limorté
  *
  *  This is free software. You can redistribute it and/or modify it under
  *  the terms of Creative Commons Attribution 3.0 United States License. 
@@ -14,32 +15,21 @@
  *
  */
 
-#include "TimerThree.h"
+#include "TimerFour.h"
 
-TimerThree Timer3;              // preinstatiate
+TimerFour Timer4;              // preinstatiate
 
-unsigned short TimerThree::pwmPeriod = 0;
-unsigned char TimerThree::clockSelectBits = 0;
-void (*TimerThree::isrCallback)() = NULL;
+unsigned short TimerFour::pwmPeriod = 0;
+unsigned char TimerFour::clockSelectBits = 0;
+void (*TimerFour::isrCallback)() = NULL;
 
 // interrupt service routine that wraps a user defined function supplied by attachInterrupt
 #if defined(__AVR__)
-ISR(TIMER3_OVF_vect)
+ISR(TIMER4_OVF_vect)
 {
-  Timer3.isrCallback();
+  Timer4.isrCallback();
 }
 
-#elif defined(__arm__) && defined(CORE_TEENSY)
-void ftm2_isr(void)
-{
-  uint32_t sc = FTM2_SC;
-  #ifdef KINETISL
-  if (sc & 0x80) FTM2_SC = sc;
-  #else
-  if (sc & 0x80) FTM2_SC = sc & 0x7F;
-  #endif
-  Timer3.isrCallback();
-}
 
 #endif
 
